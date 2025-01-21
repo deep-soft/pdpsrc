1b258c5b73133391c0e11ac428f1e248b98e19d6
Dave Plummer <dave@daveandnikki.com>
2025-01-21 06:27:29 -0800

    Bug6

---
+++ bsd/bugs/bug6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x = -10;
+    unsigned int y = 5;
+    if (x > y) {
+        printf("x is greater than y\n");
+    } else {
+        printf("x is less than y\n");
+    }
+}
+
+
